66caeaba487b5ad7afdd67fb6355c138ffec4f84
pandel24 <handelmarcelocayoportillo@gmail.com>
2026-08-26 10:25:44 -0400

    subi el ejercicio 1 de la practica 0 po favor

---
+++ Practicas Programacion/Practica_00/Ejercicio_00_01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "ingree numeros : ";
+    cin >> num1 >> num2;
+    cout << "la suma es : " << num1 + num2 << "\n la resta es : " << num1 - num2 << "\n la multiplicacion es : " << num1 * num2 << "\n la division es : " << num1 / num2;
+
+    return 0;
+}

b36e9fa34d45ab13f504405ee2cf9c52cf4fd8c4
pandel24 <handelmarcelocayoportillo@gmail.com>
2026-08-26 14:48:13 -0400

    ejricico 4 practica0

---
+++ Practicas Programacion/Practica_00/Ejercicio_00_04.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    float num1, num2, num3, num4, resultado = 0;
+    cout << "ingrese prmero numero  : ";
+    cin >> num1;
+    cout << "ingree segundo numero : ";
+    cin >> num2;
+    cout << "ingrese tercer numero  : ";
+    cin >> num3;
+    cout << "ingree cuarto numero : ";
+    cin >> num4;
+    resultado = num1 + (num2 / (num3 - num4));
+    cout.precision(2);
+    cout << resultado;
+
+    return 0;
+}

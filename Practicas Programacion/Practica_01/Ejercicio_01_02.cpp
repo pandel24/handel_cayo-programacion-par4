a535a9b881691be67fa83e6342cac6b534ca2428
pandel24 <handelmarcelocayoportillo@gmail.com>
2026-08-26 15:43:35 -0400

    Add files via upload

---
+++ Practicas Programacion/Practica_01/Ejercicio_01_02.cpp
@@ -0,0 +1,16 @@
+
+// Materia: Programación I, Paralelo 4
+// Autor: Hnadel Marcelo Cayo Portillo
+// Carrera del estudiante: Ing Mecatronica
+// Fecha creación: 10/08/2026
+#include <iostream>
+using namespace std;
+int main()
+{
+    float precio;
+    cout << "ingrese precio : ";
+    cin >> precio;
+    precio = precio * 1.13;
+    cout << precio;
+    return 0;
+}
---
+++ Practicas Programacion/Practica_01/Ejercicio_01_03.cpp
@@ -0,0 +1,21 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Hnadel Marcelo Cayo Portillo
+// Carrera del estudiante: Ing Mecatronica
+// Fecha creación: 10/08/2026
+#include <iostream>
+using namespace std;
+int main()
+{
+    int edad;
+    char sexo[10];
+    float altura;
+    cout << "ingree su edad : ";
+    cin >> edad;
+    cout << "ingree su genero : ";
+    cin >> sexo;
+    cout << "ingree su altura : ";
+    cin >> altura;
+    cout << "edad : " << edad << "\ngenero : " << sexo << "\naltura : " << altura;
+
+    return 0;
+}

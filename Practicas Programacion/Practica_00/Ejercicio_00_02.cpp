411788742424454f4c4680d7588e03a8e2e3f619
pandel24 <handelmarcelocayoportillo@gmail.com>
2026-08-26 10:33:43 -0400

    aca el segundo ejericio

---
+++ Practicas Programacion/Practica_00/Ejercicio_00_02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int edad;
+    char sexo[10];
+    float altura;
+    cout << "ingree su edad : ";
+    cin >> edad;
+    cout << "ingree su genero : ";
+    cin >> sexo;
+    cout << "ingree su altura : ";
+    cin >> altura;
+    cout << "edad : " << edad << "\ngenero : " << sexo << "\naltura : " << altura;
+
+    return 0;
+}
